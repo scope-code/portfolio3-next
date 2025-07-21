a84d519e6e34907ed703225de5d490e8a9f3add4
Yuossef Ahmed <scopecode.info@gmail.com>
2025-07-22 02:50:00 +0300

    done

---
+++ config/pages.ts
@@ -0,0 +1,74 @@
+import { ValidPages } from "./constants";
+
+type PagesConfig = {
+  [key in ValidPages]: {
+    title: string;
+    description: string;
+    metadata: {
+      title: string;
+      description: string;
+    };
+    // featuredDescription: string;
+  };
+};
+
+export const pagesConfig: PagesConfig = {
+  home: {
+    title: "Home",
+    description: "Welcome to my portfolio website.",
+    metadata: {
+      title: "Home",
+      description: "Naman Barkiya's portfolio website.",
+    },
+  },
+  skills: {
+    title: "Skills",
+    description: "Key skills that define my professional identity.",
+    metadata: {
+      title: "Skills",
+      description:
+        "Naman Barkiya's key skills that define his professional identity.",
+    },
+  },
+  experience: {
+    title: "Experience",
+    description: "Highlighting career journey and impactful projects.",
+    metadata: {
+      title: "Experience",
+      description: "Naman Barkiya's experience in building web applications.",
+    },
+  },
+  contact: {
+    title: "Contact",
+    description: "Let's connect and explore collaborations.",
+    metadata: {
+      title: "Contact",
+      description: "Contact Naman Barkiya.",
+    },
+  },
+  contributions: {
+    title: "Contributions",
+    description: "Open-source contributions and community involvement.",
+    metadata: {
+      title: "Contributions",
+      description:
+        "Naman Barkiya's open-source contributions and community involvement.",
+    },
+  },
+  resume: {
+    title: "Resume",
+    description: "Naman Barkiya's resume.",
+    metadata: {
+      title: "Resume",
+      description: "Naman Barkiya's resume.",
+    },
+  },
+  career: {
+    title: "Career Timeline",
+    description: "Professional journey and experience timeline.",
+    metadata: {
+      title: "Career Timeline",
+      description: "Naman Barkiya's professional journey and career timeline.",
+    },
+  },
+};
